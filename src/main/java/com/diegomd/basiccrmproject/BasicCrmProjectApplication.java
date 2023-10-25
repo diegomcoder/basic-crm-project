7e2f56f113dd63b4b2ade5547d53540ffac54406
Diego Medeiros <mdsidiego@gmail.com>
2023-10-25 09:23:27 -0300

    update initial project files

---
+++ src/main/java/com/diegomd/basiccrmproject/BasicCrmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.diegomd.basiccrmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegomd/basiccrmproject/BasicCrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegomd.basiccrmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
