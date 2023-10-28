c43211cbecc7b5f3f1863f7aadf9b3147922e2b7
Diego Medeiros <mdsidiego@gmail.com>
2023-10-27 22:23:30 -0300

    first instance of Client

--- src/main/java/com/diegomd/basiccrmproject/BasicCrmProjectApplication.java
+++ src/main/java/com/diegomd/basiccrmproject/BasicCrmProjectApplication.java
@@ -8,6 +8,10 @@ public class BasicCrmProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicCrmProjectApplication.class, args);
+		Client stClient = new Client("John", "Vonn", "Newmann", 12345678910L,
+				22_4525_3232L, 32, "23/09/1947", "Street 4, nt avenue",Genders.MASCULINE);
+
+		System.out.println(stClient);
 	}
 
 }
