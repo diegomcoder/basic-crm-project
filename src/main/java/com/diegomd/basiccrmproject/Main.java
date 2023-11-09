4a96727ddaf3c8447ceb573f238f939962fed0f3
diegomcoder <mdsidiego@gmail.com>
2023-11-08 21:27:06 -0300

    Test ClientBuilder instance

--- src/main/java/com/diegomd/basiccrmproject/Main.java
+++ src/main/java/com/diegomd/basiccrmproject/Main.java
@@ -8,16 +8,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+
 		Client firstClient = new Client("David", "Osborne", "Junior",
 				12345678910L, "davidosborne@some.com", 62_233_4567L,
 				"13/08/1987", "States avn 127st", Genders.MASCULINE);
-
 		System.out.printf(firstClient.toString());
+
+		ClientBuilder clientWithIdentification = new ClientBuilder().withIdentification(
+				"David","Osborne","Junior", 12345678910L,
+				"13/08/1987",Genders.MASCULINE);
 
 //		Client stClient = new Client("John", "Vonn", "Newmann", 12345678910L,
 //				22_4525_3232L, 32, "23/09/1947", "Street 4, nt avenue",Genders.MASCULINE);
 //
 //		System.out.println(stClient);
+		/*
+		Se eu não me engano, existe um método isEmpty para objetos String
+		* */
 	}
 
 }
