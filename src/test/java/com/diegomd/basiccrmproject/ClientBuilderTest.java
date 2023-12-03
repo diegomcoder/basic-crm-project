26999dba45941adf1ac9ec8da65ce0d675443b88
diegomcoder <mdsidiego@gmail.com>
2023-12-02 23:56:53 -0300

    Config Maven Tests

--- src/test/java/com/diegomd/basiccrmproject/ClientBuilderTest.java
+++ src/test/java/com/diegomd/basiccrmproject/ClientBuilderTest.java
@@ -1,20 +1,23 @@
 package com.diegomd.basiccrmproject;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClientBuilderTest extends ClientBuilder {
+public class ClientBuilderTest {
 
     ClientBuilder clientBuilderNoArgs;
     ClientBuilder clientBuilderFull;
     ClientBuilder clientBuilderWithIdentification;
     ClientBuilder clientBuilderWithContactInfo;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         clientBuilderNoArgs = new ClientBuilder();
 
         clientBuilderFull = new ClientBuilder("Marie", "Carey", "Jones",
@@ -29,187 +32,187 @@ class ClientBuilderTest extends ClientBuilder {
     }
 
     @Test
-    void testSetFirstName() {
+    public void testSetFirstName() {
         clientBuilderNoArgs.setFirstName("Clara");
-        assertEquals("Clara", clientBuilderNoArgs.getFirstName());
+        Assertions.assertEquals("Clara", clientBuilderNoArgs.getFirstName());
 
         clientBuilderFull.setFirstName("Andre");
-        assertEquals("Andre", clientBuilderFull.getFirstName());
+        Assert.assertEquals("Andre", clientBuilderFull.getFirstName());
     }
 
     @Test
-    void testSetMiddleName() {
+    public void testSetMiddleName() {
         clientBuilderNoArgs.setFirstName("Clara");
         clientBuilderNoArgs.setMiddleName("Antunes");
-        assertEquals("Antunes", clientBuilderNoArgs.getMiddleName());
+        Assertions.assertEquals("Antunes", clientBuilderNoArgs.getMiddleName());
     }
 
     @Test
-    void testSetLastName() {
+    public void testSetLastName() {
         clientBuilderNoArgs.setFirstName("Clara");
         clientBuilderNoArgs.setLastName("Borges");
-        assertEquals("Borges", clientBuilderNoArgs.getLastName());
+        Assertions.assertEquals("Borges", clientBuilderNoArgs.getLastName());
     }
 
     @Test
-    void testGetFullName() {
+    public void testGetFullName() {
         clientBuilderNoArgs.setFirstName("Alex");
         clientBuilderNoArgs.setLastName("Marcus");
-        assertEquals("Alex Marcus", clientBuilderNoArgs.getFullName());
+        Assertions.assertEquals("Alex Marcus", clientBuilderNoArgs.getFullName());
     }
 
     @Test
-    void testGetCpf() {
-        assertNull(clientBuilderNoArgs.getCpf());
-        assertEquals("789.678.543-21", clientBuilderFull.getCpf());
-        assertEquals("123.456.789-10", clientBuilderWithIdentification.getCpf());
-        assertNull(clientBuilderWithContactInfo.getCpf());
+    public void testGetCpf() {
+        Assertions.assertNull(clientBuilderNoArgs.getCpf());
+        Assertions.assertEquals("789.678.543-21", clientBuilderFull.getCpf());
+        Assertions.assertEquals("123.456.789-10", clientBuilderWithIdentification.getCpf());
+        Assertions.assertNull(clientBuilderWithContactInfo.getCpf());
     }
 
     @Test
-    void testSetCpf() {
+    public void testSetCpf() {
         clientBuilderNoArgs.setCpf("101.010.101-01");
-        assertEquals("101.010.101-01", clientBuilderNoArgs.getCpf());
+        Assertions.assertEquals("101.010.101-01", clientBuilderNoArgs.getCpf());
 
         clientBuilderFull.setCpf("987.654.321-23");
-        assertEquals("987.654.321-23", clientBuilderFull.getCpf());
+        Assertions.assertEquals("987.654.321-23", clientBuilderFull.getCpf());
 
         clientBuilderWithIdentification.setCpf("345.678.999-11");
-        assertEquals("345.678.999-11", clientBuilderWithIdentification.getCpf());
+        Assertions.assertEquals("345.678.999-11", clientBuilderWithIdentification.getCpf());
 
         clientBuilderWithContactInfo.setCpf("140.160.234-80");
-        assertEquals("140.160.234-80", clientBuilderWithContactInfo.getCpf());
+        Assertions.assertEquals("140.160.234-80", clientBuilderWithContactInfo.getCpf());
     }
 
     @Test
-    void testGetGenderTreatment() {
-        assertEquals("", clientBuilderNoArgs.getGenderTreatment());
-        assertEquals("Mrs.", clientBuilderFull.getGenderTreatment());
-        assertEquals("Mr.", clientBuilderWithIdentification.getGenderTreatment());
-        assertEquals("", clientBuilderWithContactInfo.getGenderTreatment());
+    public void testGetGenderTreatment() {
+        Assertions.assertEquals("", clientBuilderNoArgs.getGenderTreatment());
+        Assertions.assertEquals("Mrs.", clientBuilderFull.getGenderTreatment());
+        Assertions.assertEquals("Mr.", clientBuilderWithIdentification.getGenderTreatment());
+        Assertions.assertEquals("", clientBuilderWithContactInfo.getGenderTreatment());
     }
 
     @Test
-    void testGetEmail() {
-        assertNull(clientBuilderNoArgs.getEmail());
-        assertEquals("marie.carey27@fakemail.com", clientBuilderFull.getEmail());
-        assertNull(clientBuilderWithIdentification.getEmail());
-        assertEquals("davidosborne@some.com", clientBuilderWithContactInfo.getEmail());
+    public void testGetEmail() {
+        Assertions.assertNull(clientBuilderNoArgs.getEmail());
+        Assertions.assertEquals("marie.carey27@fakemail.com", clientBuilderFull.getEmail());
+        Assertions.assertNull(clientBuilderWithIdentification.getEmail());
+        Assertions.assertEquals("davidosborne@some.com", clientBuilderWithContactInfo.getEmail());
     }
 
     @Test
-    void testSetEmail() {
+    public void testSetEmail() {
         clientBuilderNoArgs.setEmail("noargs.cb@fakemail.com");
-        assertEquals("noargs.cb@fakemail.com", clientBuilderNoArgs.getEmail());
+        Assertions.assertEquals("noargs.cb@fakemail.com", clientBuilderNoArgs.getEmail());
 
         clientBuilderFull.setEmail("full.cb@fakemail.com");
-        assertEquals("full.cb@fakemail.com", clientBuilderFull.getEmail());
+        Assertions.assertEquals("full.cb@fakemail.com", clientBuilderFull.getEmail());
 
         clientBuilderWithIdentification.setEmail("identification.cb@fakemail.com");
-        assertEquals("identification.cb@fakemail.com", clientBuilderWithIdentification.getEmail());
+        Assertions.assertEquals("identification.cb@fakemail.com", clientBuilderWithIdentification.getEmail());
 
         clientBuilderWithContactInfo.setEmail("contact.cb@fakemail.com");
-        assertEquals("contact.cb@fakemail.com", clientBuilderWithContactInfo.getEmail());
+        Assertions.assertEquals("contact.cb@fakemail.com", clientBuilderWithContactInfo.getEmail());
     }
 
     @Test
-    void testGetPhoneNumber() {
-        assertNull(clientBuilderNoArgs.getPhoneNumber());
-        assertEquals("(970) 884-8728", clientBuilderFull.getPhoneNumber());
-        assertNull(clientBuilderWithIdentification.getPhoneNumber());
-        assertEquals("(62) 233 4567", clientBuilderWithContactInfo.getPhoneNumber());
+    public void testGetPhoneNumber() {
+        Assertions.assertNull(clientBuilderNoArgs.getPhoneNumber());
+        Assertions.assertEquals("(970) 884-8728", clientBuilderFull.getPhoneNumber());
+        Assertions.assertNull(clientBuilderWithIdentification.getPhoneNumber());
+        Assertions.assertEquals("(62) 233 4567", clientBuilderWithContactInfo.getPhoneNumber());
     }
 
     @Test
-    void testSetPhoneNumber() {
+    public void testSetPhoneNumber() {
         clientBuilderNoArgs.setPhoneNumber("(52) 436 34567");
-        assertEquals("(52) 436 34567", clientBuilderNoArgs.getPhoneNumber());
+        Assertions.assertEquals("(52) 436 34567", clientBuilderNoArgs.getPhoneNumber());
 
         clientBuilderFull.setPhoneNumber("(14) 863 2345 23423");
-        assertEquals("(14) 863 2345 23423", clientBuilderFull.getPhoneNumber());
+        Assertions.assertEquals("(14) 863 2345 23423", clientBuilderFull.getPhoneNumber());
 
         clientBuilderWithIdentification.setPhoneNumber("(55) 98227 8903");
-        assertEquals("(55) 98227 8903", clientBuilderWithIdentification.getPhoneNumber());
+        Assertions.assertEquals("(55) 98227 8903", clientBuilderWithIdentification.getPhoneNumber());
 
         clientBuilderWithContactInfo.setPhoneNumber(null);
-        assertNull(clientBuilderWithContactInfo.getPhoneNumber());
+        Assertions.assertNull(clientBuilderWithContactInfo.getPhoneNumber());
     }
 
     @Test
-    void testGetAge() {
+    public void testGetAge() {
         clientBuilderNoArgs.setDateOfBirth("02/01/2004");
-        assertEquals(19, clientBuilderNoArgs.getAge());
+        Assertions.assertEquals(19, clientBuilderNoArgs.getAge());
 
-        assertEquals(25, clientBuilderFull.getAge());
-        assertEquals(36, clientBuilderWithIdentification.getAge());
+        Assertions.assertEquals(25, clientBuilderFull.getAge());
+        Assertions.assertEquals(36, clientBuilderWithIdentification.getAge());
 
         clientBuilderWithContactInfo.setDateOfBirth("15/11/1954");
-        assertEquals(69, clientBuilderWithContactInfo.getAge());
+        Assertions.assertEquals(69, clientBuilderWithContactInfo.getAge());
     }
 
     @Test
-    void testGetDateOfBirth() {
-        assertNull(clientBuilderNoArgs.getDateOfBirth());
-        assertEquals("22/05/1998", clientBuilderFull.getDateOfBirth());
-        assertEquals("13/08/1987", clientBuilderWithIdentification.getDateOfBirth());
-        assertNull(clientBuilderWithContactInfo.getDateOfBirth());
+    public void testGetDateOfBirth() {
+        Assertions.assertNull(clientBuilderNoArgs.getDateOfBirth());
+        Assertions.assertEquals("22/05/1998", clientBuilderFull.getDateOfBirth());
+        Assertions.assertEquals("13/08/1987", clientBuilderWithIdentification.getDateOfBirth());
+        Assertions.assertNull(clientBuilderWithContactInfo.getDateOfBirth());
     }
 
     @Test
-    void testSetDateOfBirth() {
+    public void testSetDateOfBirth() {
         clientBuilderNoArgs.setDateOfBirth("28/12/1995");
-        assertEquals("28/12/1995", clientBuilderNoArgs.getDateOfBirth());
+        Assertions.assertEquals("28/12/1995", clientBuilderNoArgs.getDateOfBirth());
 
         clientBuilderFull.setDateOfBirth("14/07/1987");
-        assertEquals("14/07/1987", clientBuilderFull.getDateOfBirth());
+        Assertions.assertEquals("14/07/1987", clientBuilderFull.getDateOfBirth());
 
         clientBuilderWithIdentification.setDateOfBirth("03/11/2005");
-        assertEquals("03/11/2005", clientBuilderWithIdentification.getDateOfBirth());
+        Assertions.assertEquals("03/11/2005", clientBuilderWithIdentification.getDateOfBirth());
 
         clientBuilderWithContactInfo.setDateOfBirth("12/04/1948");
-        assertEquals("12/04/1948", clientBuilderWithContactInfo.getDateOfBirth());
+        Assertions.assertEquals("12/04/1948", clientBuilderWithContactInfo.getDateOfBirth());
     }
 
     @Test
-    void testGetAddress() {
-        assertNull(clientBuilderNoArgs.getAddress());
-        assertEquals("1560 Pennsylvania Avenue", clientBuilderFull.getAddress());
-        assertNull(clientBuilderWithIdentification.getAddress());
-        assertEquals("States avn 127st", clientBuilderWithContactInfo.getAddress());
+    public void testGetAddress() {
+        Assertions.assertNull(clientBuilderNoArgs.getAddress());
+        Assertions.assertEquals("1560 Pennsylvania Avenue", clientBuilderFull.getAddress());
+        Assertions.assertNull(clientBuilderWithIdentification.getAddress());
+        Assertions.assertEquals("States avn 127st", clientBuilderWithContactInfo.getAddress());
     }
 
     @Test
-    void testSetAddress() {
+    public void testSetAddress() {
         clientBuilderNoArgs.setAddress("1974 Maple Lane");
-        assertEquals("1974 Maple Lane", clientBuilderNoArgs.getAddress());
+        Assertions.assertEquals("1974 Maple Lane", clientBuilderNoArgs.getAddress());
 
         clientBuilderFull.setAddress("2634 Mayo Street");
-        assertEquals("2634 Mayo Street", clientBuilderFull.getAddress());
+        Assertions.assertEquals("2634 Mayo Street", clientBuilderFull.getAddress());
 
         clientBuilderWithIdentification.setAddress("3650 Hickman Street");
-        assertEquals("3650 Hickman Street", clientBuilderWithIdentification.getAddress());
+        Assertions.assertEquals("3650 Hickman Street", clientBuilderWithIdentification.getAddress());
 
         clientBuilderWithContactInfo.setAddress("4458 Tully Street");
-        assertEquals("4458 Tully Street", clientBuilderWithContactInfo.getAddress());
+        Assertions.assertEquals("4458 Tully Street", clientBuilderWithContactInfo.getAddress());
     }
 
     @Test
-    void testExceptionThrowing() {
+    public void testExceptionThrowing() {
         IllegalArgumentException thrown1 = assertThrows(
                 IllegalArgumentException.class,
                 () -> clientBuilderNoArgs.setFirstName(null)
         );
-        assertTrue(thrown1.getMessage().contains("First name can't be empty or null"));
+        Assertions.assertTrue(thrown1.getMessage().contains("First name can't be empty or null"));
 
         IllegalArgumentException thrown2 = assertThrows(
                 IllegalArgumentException.class,
                 () -> clientBuilderNoArgs.getAge()
         );
-        assertTrue(thrown2.getMessage().contains("Date of birth can't be null"));
+        Assertions.assertTrue(thrown2.getMessage().contains("Date of birth can't be null"));
     }
 
-    @AfterEach
-    void tearDown() {
+    @After
+    public void tearDown() {
         clientBuilderNoArgs = null;
         clientBuilderFull = null;
         clientBuilderWithIdentification = null;
